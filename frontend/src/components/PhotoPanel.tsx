4c8abfa69b82fbc64bd39bb48df2add63536ffe4
aalnabi <98138326+aalnabi@users.noreply.github.com>
2025-09-25 13:52:26 -0400

    Condensed PhotoCard.tsx and PhotoUploader.tsx into PhotoPanel.tsx

--- frontend/src/components/PhotoPanel.tsx
+++ frontend/src/components/PhotoPanel.tsx
@@ -1,9 +1,8 @@
 import React, { useState, useEffect } from "react";
-import PosterCard from "./PhotoCard";
 
 const LOCAL_STORAGE_KEY = "uploadedPhoto";
 
-const PhotoUploader: React.FC = () => {
+const PhotoPanel: React.FC = () => {
   const [photoSrc, setPhotoSrc] = useState<string | null>(null);
 
   useEffect(() => {
@@ -36,6 +35,7 @@ const PhotoUploader: React.FC = () => {
 
   return (
     <div className="flex flex-col items-center space-y-2">
+      {/* Buttons */}
       <div className="flex space-x-2">
         <button
           type="button"
@@ -67,9 +67,23 @@ const PhotoUploader: React.FC = () => {
         onChange={handleFileChange}
       />
 
-      <PosterCard src={photoSrc} />
+      <div className="min-w-[380px] sm:min-w-[500px] max-w-sm w-full">
+        <div className="aspect-[1/1.414] bg-white border border-gray-900 shadow-lg rounded-lg overflow-hidden flex items-center justify-center">
+          {photoSrc ? (
+            <img
+              src={photoSrc}
+              alt="Uploaded"
+              className="w-full h-full object-contain"
+            />
+          ) : (
+            <div className="flex items-center justify-center text-gray-400 text-sm">
+              Start by uploading an image!
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
 
-export default PhotoUploader;
+export default PhotoPanel;
